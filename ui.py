8cf549adcd5fcee6e5ae412f8c5f3ddd23ee8bc8
LEWIS <jassylewis70@gmail.com>
2023-10-03 22:02:41 +0300

    new updates

--- ui.py
+++ ui.py
@@ -1,12 +1,44 @@
 from tkinter import *
 from oop import MoneyTracker
 from tkinter import messagebox
+from PIL import Image, ImageTk
+
+
+def highlight_button():
+    add_income_button.config(bg="lightgreen")
+
+def reset_button_color():
+    add_income_button.config(bg="green")
+
+def on_hover(event):
+    highlight_button()
+
+def on_leave(event):
+    reset_button_color()
 
 window = Tk()
 window.title("MONEY TRACKER APP")
 window.minsize(width=400, height=300)
 
+icon = Image.open("money.jpg")
+window.iconphoto(True, ImageTk.PhotoImage(icon))
+
+bg_image = Image.open("background.jpg")
+bg_image = bg_image.resize((400, 300))
+bg_photo = ImageTk.PhotoImage(bg_image)
+
+bg_label = Label(window, image=bg_photo)
+bg_label.place(relwidth=1, relheight=1)
+bg_label.image = bg_photo
+
 money_tracker = MoneyTracker()
+
+
+frame1 = Frame(window, bg="SystemButtonFace")
+frame1.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")  # Use grid layout
+
+frame2 = Frame(window, bg="SystemButtonFace")
+frame2.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")  # Use grid layout
 
 
 def update_total_income_label(total_income):
@@ -45,20 +77,25 @@ def expense():
 # income/expense entry that allows the user to enter the income and expense
 
 
-income_label = Label(window, text="Income:")
+income_label = Label(frame1, text="Income:", font=("Helvetica", 12))
 income_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-income_entry = Entry(window, width=20)
+income_entry = Entry(frame1, width=20)
 income_entry.grid(row=1, column=1, padx=10, pady=5)
-expense_label = Label(window, text="Expense:")
+expense_label = Label(frame2, text="Expense:", font=("Helvetica", 12))
 expense_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-expense_entry = Entry(window, width=20)
+expense_entry = Entry(frame2, width=20)
 expense_entry.grid(row=2, column=1, padx=10, pady=5, sticky="w")
 # income/expense button which when clicked it add both income and expense info
-add_income_button = Button(window, text="Add Income", command=income)
+add_income_button = Button(frame1, text="Add Income", command=income, bg="green", fg="white", font=("Helvetica", 12))
 add_income_button.grid(row=1, column=2, padx=10, pady=5)
 
-add_expense_button = Button(window, text="Add Expense", command=expense)
+
+add_expense_button = Button(frame2, text="Add Expense", command=expense, bg="green", fg="white", font=("Helvetica", 12))
 add_expense_button.grid(row=2, column=2, padx=10, pady=5)
+
+
+add_income_button.bind("<Enter>", on_hover)
+add_income_button.bind("<Leave>", on_leave)
 
 # A label to display the balance
 balance_label = Label(window, text=f"Balance: ${money_tracker.get_balance()}")
